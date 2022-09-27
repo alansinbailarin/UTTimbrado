cca741989c2109b85a232e0e486a5037d857335d
alanelbailarin <031912556@ut-tijuana.edu.mx>
2022-09-26 20:11:17 -0700

    Mas cambios

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,6 +33,7 @@ const Sidebar = ({children}) => {
         <div className="container">
            <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
                <div className="top_section">
+                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">UTT</h1>
                    <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
                        <FaBars onClick={toggle}/>
                    </div>
