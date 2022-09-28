d2689804e169d1cffd6e254a884a72173d4e1b8e
alanelbailarin <031912556@ut-tijuana.edu.mx>
2022-09-27 18:46:13 -0700

    otros cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,7 @@ import React from "react"
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="searching">
-                <input className="search-input" id="search-alumn" type="search" placeholder="Escribe un nombre..."/>
-                <button className="search-btn" type="submit" htmlFor="search-alumn">Buscar</button>
-            </div>
-            <div className="right-side">
-            <img src="uttimbrado/resources/logo.png" alt="" className="profile-img-admin"/>
-            </div>
+            <input className="search-input" id="search-alumn" type="search" placeholder="Escribe un nombre..."/>
         </div>
     )
 }
